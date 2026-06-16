5b4ce89d056122b4c0f610a061faf28227571d50
Wilson2002-na <84163186+Wilson2002-na@users.noreply.github.com>
2026-06-16 17:16:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # =========================
 # INSTALL (run once)
 # =========================
-# pip install yt-dlp openai-whisper deep-translator moviepy elevenlabs
+# pip install yt-dlp openai-whisper deep-translator moviepy gtts
+
 
 # =========================
 # IMPORTS
@@ -11,9 +12,7 @@ import yt_dlp
 import whisper
 from deep_translator import GoogleTranslator
 from moviepy import VideoFileClip, AudioFileClip
-
-from elevenlabs.client import ElevenLabs
-from elevenlabs import save
+from gtts import gTTS
 
 
 # =========================
@@ -61,6 +60,7 @@ english_sentences = []
 for item in segments:
     english_sentences.append(item["text"])
 
+
 print("\nEnglish Text:")
 print(english_sentences)
 
@@ -71,7 +71,10 @@ print(english_sentences)
 
 print("\nTranslating to Tamil...")
 
-translator = GoogleTranslator(source="en", target="ta")
+translator = GoogleTranslator(
+    source="en",
+    target="ta"
+)
 
 tamil_sentences = []
 
@@ -79,34 +82,30 @@ for sentence in english_sentences:
     tamil = translator.translate(sentence)
     tamil_sentences.append(tamil)
 
+
 tamil_text = " ".join(tamil_sentences)
+
 
 print("\nTamil Text:")
 print(tamil_text)
 
 
 # =========================
-# 5. TEXT → SPEECH (ELEVENLABS)
+# 5. TEXT → SPEECH (gTTS)
 # =========================
 
-print("\nCreating Tamil voice with ElevenLabs...")
+print("\nCreating Tamil voice with gTTS...")
 
-client = ElevenLabs(
-    api_key="sk_155bd03b99285b4dfecfb19008f1f347bc5e113f446329e7"   # 🔴 PUT YOUR KEY HERE
-)
 
-# You can change voice_id anytime
-voice_id = "pNInz6obpgDQGcFmaJgB"
-
-audio = client.text_to_speech.convert(
-    voice_id=voice_id,
+tts = gTTS(
     text=tamil_text,
-    model_id="eleven_multilingual_v2"
+    lang="ta"
 )
 
-save(audio, "tamil_audio.mp3")
+tts.save("tamil_audio.mp3")
 
-print("ElevenLabs audio created ✅")
+
+print("Tamil audio created ✅")
 
 
 # =========================
@@ -115,16 +114,23 @@ print("ElevenLabs audio created ✅")
 
 print("\nCreating Tamil dubbed video...")
 
+
 video = VideoFileClip("video.mp4")
-audio = AudioFileClip("tamil_audio.mp3")
+
+audio = AudioFileClip(
+    "tamil_audio.mp3"
+)
+
 
 final_video = video.with_audio(audio)
+
 
 final_video.write_videofile(
     "Tamil_Dubbed.mp4",
     codec="libx264",
     audio_codec="aac"
 )
+
 
 print("\nDONE ✅")
 print("Tamil Dubbed Video: Tamil_Dubbed.mp4")
