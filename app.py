7bf4af2bd42f6fd45bc2beb65ea2d252172578e3
Wilson2002-na <84163186+Wilson2002-na@users.noreply.github.com>
2026-07-10 20:14:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 #files upload
-uploaded=st.file_uploader[
+uploaded=st.file_uploader(
 "upload a video",
 type=["mp4","avi","mov","mkv","webm"]
-  ]
+)
+if uploaded_video is not None:
+  st.video(uploaded_video)
