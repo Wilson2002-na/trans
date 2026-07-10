173f3a1fa7be2ad935f6fc24054de2ca856e8198
Wilson2002-na <84163186+Wilson2002-na@users.noreply.github.com>
2026-07-10 19:54:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 #files upload
-uploaded=st.fileuploader("
-upload a video,
+uploaded=st.fileuploader(
+"upload a video",
 type=["mp4","avi","mov","mkv","webm"]
 ")
 # download button
